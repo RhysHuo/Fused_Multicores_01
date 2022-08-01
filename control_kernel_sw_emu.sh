12fe69761b1df2632be90f48bdc0681ce174a126
RhysHuo <RhysHuo_1120272267@163.com>
2022-08-01 15:44:07 +0100

    Update control_kernel_sw_emu.sh

--- control_kernel_sw_emu.sh
+++ control_kernel_sw_emu.sh
@@ -1,8 +1,8 @@
-git clone https://github.com/RhysHuo/Fused_Multicores.git
-cd Fused_Multicores
+git clone https://github.com/RhysHuo/Fused_Multicores_01.git
+cd Fused_Multicores_01
 cp kernelMatrixmult.cpp ..
 cp kernelMatrixmult.h ..
 cd ..
-rm -rf Fused_Multicores
+rm -rf Fused_Multicores_01
 v++ -t sw_emu --platform xilinx_u250_gen3x16_xdma_4_1_202210_1 -c -k mmult_top -o'kernelmult.sw_emu.xo' kernelMatrixmult.cpp kernelMatrixmult.h xcl2.hpp
 v++ -t sw_emu --platform xilinx_u250_gen3x16_xdma_4_1_202210_1 --link kernelmult.sw_emu.xo --config mmult_top.cfg -o'kernelmult.sw_emu.xclbin'
