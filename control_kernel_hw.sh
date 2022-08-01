b040eede1835f7ba5b4870ecedc1ac07aa2f8b27
RhysHuo <RhysHuo_1120272267@163.com>
2022-08-01 15:43:31 +0100

    Update control_kernel_hw.sh

--- control_kernel_hw.sh
+++ control_kernel_hw.sh
@@ -1,8 +1,8 @@
-git clone https://github.com/RhysHuo/Fused_Multicores.git
-cd Fused_Multicores
+git clone https://github.com/RhysHuo/Fused_Multicores_01.git
+cd Fused_Multicores_01
 cp kernelMatrixmult.cpp ..
 cp kernelMatrixmult.h ..
 cd ..
-rm -rf Fused_Multicores
+rm -rf Fused_Multicores_01
 v++ -t hw --platform xilinx_u250_gen3x16_xdma_4_1_202210_1 -c -k mmult_top -o'kernelmult.hw.xo' kernelMatrixmult.cpp kernelMatrixmult.h xcl2.hpp
 v++ -t hw --platform xilinx_u250_gen3x16_xdma_4_1_202210_1 --link kernelmult.hw.xo --config mmult_top.cfg -o'kernelmult.hw.xclbin'
